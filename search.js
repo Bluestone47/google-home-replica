e6cde8705ff578e7ccaf0ba82808eb113b05b1e3
XIAN DONG <dongxian.bruce@gmail.com>
2019-12-21 21:21:45 +0800

    add jquery

--- search.js
+++ search.js
@@ -1,2 +1,9 @@
 const searchBar = document.getElementsByClassName('search-bar')
 
+$("input").focus(() => {
+    $(".dropdown").show()
+});
+
+$("input").blur(function () {
+    $(".dropdown").hide()
+});
