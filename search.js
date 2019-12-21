19d63570cfcb97c9b97a88d3bad16e41f84ad720
XIAN DONG <dongxian.bruce@gmail.com>
2019-12-22 07:59:27 +0800

    show hide buttons

--- search.js
+++ search.js
@@ -1,9 +1,12 @@
 const searchBar = document.getElementsByClassName('search-bar')
 
-$("input").focus(() => {
-    $(".dropdown").show()
+$('input').focus(() => {
+    $('.dropdown').show()
+    $('.search-button-container').hide()
+    $('#dropdown-button').show()
 });
 
-$("input").blur(function () {
-    $(".dropdown").hide()
+$('input').blur(function () {
+    $('.dropdown').hide()
+    $('.search-button-container').show()
 });
